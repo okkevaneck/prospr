009e095ec47319b120465499891b6aef906f2093
Okke van Eck <okke.van.eck@gmail.com>
2021-03-12 13:50:20 +0100

    New setup version, not tested

--- setup.py
+++ setup.py
@@ -4,27 +4,25 @@ File:           setup.py
 Description:    This file contains the setup required for distributing the
                 package on PyPi.org.
 """
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-from __future__ import print_function
-import sys
 
+__version__ = "0.1a12"
 
-try:
-    from skbuild import setup
-except ImportError:
-    print(
-        "Please update pip, you need pip 10 or greater,\n"
-        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
-        file=sys.stderr,
-    )
-    raise
+ext_modules = [
+    Pybind11Extension("prospr",
+                      ["prospr/core/core_module.cpp"],
+                      define_macros=[("VERSION_INFO", __version__)],
+                      ),
+]
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="prospr",
-    version="0.1a11",
+    version=__version__,
     author="okkevaneck",
     description="A toolbox for protein folding with Python.",
     long_description=long_description,
@@ -32,9 +30,9 @@ setup(
     url="https://github.com/okkevaneck/prospr",
     license_file="LICENSE",
     license="LGPLv3",
-    packages=["core"],
-    package_dir={"": "prospr"},
-    cmake_install_dir="prospr/core",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    # package_dir={"": "prospr"},
     platforms=["any"],
     python_requires=">=3.6",
     zip_safe=False,
@@ -57,61 +55,3 @@ setup(
     keywords=["prospr protein structure prediction toolbox python c++ swig " + \
               "cmake extension heuristics pypi package"],
 )
-
-
-
-
-
-#
-# import sys
-# from pathlib import Path
-# from setuptools import setup
-# from cmake_build_extension import BuildExtension, CMakeExtension
-#
-#
-# with open("README.md", "r") as f:
-#     long_description = f.read()
-#
-# setup(
-#     name="prospr",
-#     version="0.1a11",
-#     author="okkevaneck",
-#     description="A toolbox for protein folding with Python.",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/okkevaneck/prospr",
-#     license_file="LICENSE",
-#     license="LGPLv3",
-#     platforms=["any"],
-#     python_requires=">=3.6",
-#     ext_modules=[
-#         CMakeExtension(name="prospr",
-#                        install_prefix="prospr",
-#                        source_dir=str(Path(".").absolute()),
-#                        cmake_configure_options=[
-#                            f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
-#                            "-DCALL_FROM_SETUP_PY:BOOL=ON",
-#                            "-DBUILD_SHARED_LIBS:BOOL=OFF",
-#                        ]),
-#     ],
-#     cmdclass=dict(build_ext=BuildExtension),
-#     zip_safe=False,
-#     install_requires=[ # TODO: Add version pinning.
-#         "matplotlib",
-#         "seaborn",
-#         "numpy",
-#         "pandas"
-#     ],
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "Development Status :: 2 - Pre-Alpha",
-#         "License :: OSI Approved :: GNU Lesser General Public License v3 "
-#         "(LGPLv3)",
-#         "Operating System :: OS Independent",
-#         "Topic :: Education",
-#         "Topic :: Scientific/Engineering",
-#         "Topic :: Scientific/Engineering :: Bio-Informatics",
-#     ],
-#     keywords=["prospr protein structure prediction toolbox python c++ swig " + \
-#               "cmake extension heuristics pypi package"],
-# )
