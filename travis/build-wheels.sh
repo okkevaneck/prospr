0745f3a308337a182f524cd1e2add4781e8904b5
Okke van Eck <okke.van.eck@gmail.com>
2021-03-15 13:58:56 +0100

    Reworked build wheels script

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -16,7 +16,7 @@ yum install -y atlas-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
 
@@ -27,6 +27,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install prospr --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" prospr)
 done
