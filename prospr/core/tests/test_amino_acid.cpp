4b2d324a46efe94a384048f8e2d369b7137efb66
okkevaneck <okke.van.eck@gmail.com>
2022-09-09 13:22:04 +0200

    Finished test script for amino acids

--- prospr/core/tests/test_amino_acid.cpp
+++ prospr/core/tests/test_amino_acid.cpp
@@ -3,10 +3,35 @@
  */
 
 #include <iostream>
+#include <assert.h>
+#include "../src/amino_acid.hpp"
 
 
 int main(int argc, char* argv[]) {
-    std::cout << "In main!\n";
+    /* Test creation of amino acid. */
+    AminoAcid* new_aa = new AminoAcid('H', 0, 0, 0);
+
+    /* Test getters. */
+    char type = new_aa->get_type();
+    assert (type == 'H');
+
+    int idx = new_aa->get_index();
+    assert (idx == 0);
+
+    int prev_move = new_aa->get_prev_move();
+    assert (prev_move == 0);
+
+    int next_move = new_aa->get_next_move();
+    assert (next_move == 0);
+
+    /* Test setters. */
+    new_aa->set_prev_move(1);
+    prev_move = new_aa->get_prev_move();
+    assert (prev_move == 1);
+
+    new_aa->set_next_move(1);
+    next_move = new_aa->get_next_move();
+    assert (next_move == 1);
 
     return 0;
 }
