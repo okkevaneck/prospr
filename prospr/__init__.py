aaf7309fd7362327b6804a57c4b2aeaa5bb015c5
okkevaneck <okke.van.eck@gmail.com>
2022-10-05 19:12:28 +0200

    Added dataset tests, fix small import bug, added license comments

--- prospr/__init__.py
+++ prospr/__init__.py
@@ -1,5 +1,5 @@
 from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
-from datasets import load_vanEck250, load_vanEck1000
+from .datasets import load_vanEck250, load_vanEck1000
 
 __all__ = [
     "AminoAcid",
