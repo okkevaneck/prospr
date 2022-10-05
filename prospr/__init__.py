305e7a7f040f3fafc8cee9c20c48a48a2963e7a8
okkevaneck <okke.van.eck@gmail.com>
2022-10-05 19:01:49 +0200

    Fixed imports

--- prospr/__init__.py
+++ prospr/__init__.py
@@ -1,3 +1,11 @@
-from prospr_core import Protein, depth_first, depth_first_bnb
+from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
+from datasets import load_vanEck250, load_vanEck1000
 
-__all__ = ["Protein", "depth_first", "depth_first_bnb"]
+__all__ = [
+    "AminoAcid",
+    "Protein",
+    "depth_first",
+    "depth_first_bnb",
+    "load_vanEck250",
+    "load_vanEck1000",
+]
