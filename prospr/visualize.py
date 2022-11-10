42783a1178a874bfb4cebca44b074beb8527d95f
Okke van Eck <okke.van.eck@gmail.com>
2022-11-10 16:43:29 +0100

    Fixed paper style

--- prospr/visualize.py
+++ prospr/visualize.py
@@ -22,7 +22,7 @@ def _plot_aminos_2d_basic(protein, df, ax):
     Plot amino acids in basic style in a 2D figure.
     :param Protein      protein:    Protein object to plot the hash of.
     :param DataFrame    df:         DataFrame with all ordered positions.
-=    :param Axes         ax:         Axis to plot on.
+    :param Axes         ax:         Axis to plot on.
     """
     ax.plot(df["x"], df["y"], color="black", alpha=0.65, zorder=1)
     sns.scatterplot(
@@ -54,6 +54,58 @@ def _plot_aminos_2d_basic(protein, df, ax):
         )
 
 
+def _plot_aminos_2d_paper(protein, df, ax):
+    """
+    Plot amino acids in basic style in a 2D figure.
+    :param Protein      protein:    Protein object to plot the hash of.
+    :param DataFrame    df:         DataFrame with all ordered positions.
+    :param Axes         ax:         Axis to plot on.
+    """
+    # Split dataframe on amino acid type.
+    df_H = df.loc[df["Type"] == "H"]
+    df_P = df.loc[df["Type"] == "P"]
+
+    ax.plot(df["x"], df["y"], color="black", alpha=0.65, zorder=1)
+    sns.scatterplot(
+        x="x",
+        y="y",
+        data=df_H,
+        marker="o",
+        edgecolor="royalblue",
+        s=80,
+        zorder=2,
+        ax=ax,
+    )
+    sns.scatterplot(
+        x="x",
+        y="y",
+        data=df_P,
+        marker="o",
+        facecolor='white',
+        edgecolor="orange",
+        linewidth=2,
+        s=80,
+        zorder=2,
+        ax=ax,
+    )
+
+    # Plot dotted lines between the aminos that increase the stability.
+    pairs = get_scoring_pairs(protein)
+
+    for pos1, pos2 in pairs:
+        ax.plot(
+            [pos1[0], pos2[0]],
+            [pos1[1], pos2[1]],
+            linestyle=":",
+            color="indianred",
+            alpha=0.9,
+            zorder=1,
+            lw=1.5,
+        )
+
+    ax.axis('off')
+
+
 def _plot_protein_2d(protein, style, ax):
     """
     :param Protein      protein:    Protein object to plot the hash of.
@@ -67,11 +119,14 @@ def _plot_protein_2d(protein, style, ax):
     )
     df = df.astype({"x": "int32", "y": "int32"})
 
-    # Plot amino acids in basic style.
-    _plot_aminos_2d_basic(protein, df, ax)
+    # Plot amino acids in the selected style.
+    if style == "paper":
+        _plot_aminos_2d_paper(protein, df, ax)
+    else:
+        ax.set_title(f"2D conformation with {protein.score} energy")
+        _plot_aminos_2d_basic(protein, df, ax)
 
     # Set axis labels.
-    ax.set_title(f"2D conformation with {protein.score} energy")
     ax.set_xlabel("x-axis", fontsize=13)
     ax.set_ylabel("y-axis", fontsize=13)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
@@ -85,7 +140,7 @@ def _plot_protein_2d(protein, style, ax):
         color="indianred",
         linestyle=":",
         alpha=0.9,
-        label="Contact",
+        label="Bond",
         lw=1.5,
     )
     handles.append(score_patch)
@@ -94,6 +149,56 @@ def _plot_protein_2d(protein, style, ax):
 
 
 def _plot_aminos_3d_basic(protein, df, ax):
+    """
+    Plot amino acids in basic style in a 3D figure.
+    :param Protein      protein:    Protein object to plot the hash of.
+    :param DataFrame    df:         DataFrame with all ordered positions.
+    :param Axes         ax:         Axis to plot on.
+    """
+    # Split dataframe on amino acid type.
+    df_H = df.loc[df["Type"] == "H"]
+    df_P = df.loc[df["Type"] == "P"]
+
+    # Plot the aminos connected with an opaque line.
+    ax.scatter(
+        df_H["x"],
+        df_H["y"],
+        df_H["z"],
+        c="royalblue",
+        marker="o",
+        depthshade=False,
+        s=60,
+        label="H",
+    )
+    ax.scatter(
+        df_P["x"],
+        df_P["y"],
+        df_P["z"],
+        c="orange",
+        marker="s",
+        depthshade=False,
+        s=60,
+        label="P",
+    )
+    ax.plot(df["x"], df["y"], df["z"], color="black", alpha=0.65, zorder=1)
+
+    # Plot dotted lines between the aminos that increase the stability.
+    pairs = get_scoring_pairs(protein)
+
+    for pos1, pos2 in pairs:
+        ax.plot(
+            [pos1[0], pos2[0]],
+            [pos1[1], pos2[1]],
+            [pos1[2], pos2[2]],
+            linestyle=":",
+            color="indianred",
+            alpha=0.9,
+            zorder=1,
+            lw=1.5,
+        )
+
+
+def _plot_aminos_3d_paper(protein, df, ax):
     """
     Plot amino acids in basic style in a 3D figure.
     :param Protein      protein:    Protein object to plot the hash of.
@@ -155,8 +260,11 @@ def _plot_protein_3d(protein, style, ax):
     )
     df = df.astype({"x": "int32", "y": "int32", "z": "int32"})
 
-    # Plot amino acids in basic style.
-    _plot_aminos_3d_basic(protein, df, ax)
+    # Plot amino acids in the selected style.
+    if style == "paper":
+        _plot_aminos_3d_paper(protein, df, ax)
+    else:
+        _plot_aminos_3d_basic(protein, df, ax)
 
     # Set axis labels and tics.
     ax.set_title(f"3D conformation with {protein.score} energy")
@@ -175,7 +283,7 @@ def _plot_protein_3d(protein, style, ax):
         color="indianred",
         linestyle=":",
         alpha=0.9,
-        label="Contact",
+        label="Bond",
         lw=1.5,
     )
     handles.append(score_patch)
@@ -191,8 +299,11 @@ def plot_protein(protein, style="basic", ax=None):
     :param Axes         ax:         Axis to plot Protein on.
     """
     if ax is None:
-        fig = plt.figure(figsize=(6, 5))
-        sns.set_style("whitegrid")
+        if style == "paper":
+            fig = plt.figure(figsize=(2,2))
+        else:
+            fig = plt.figure(figsize=(5,6))
+            sns.set_style("whitegrid")
 
     # Plot data according to used dimension.
     if protein.dim == 2:
--- test_visualization.py
+++ test_visualization.py
@@ -3,5 +3,4 @@ from prospr import Protein, depth_first_bnb, plot_protein
 if __name__ == '__main__':
     p = Protein("HPPHPPHPPH", model="HP")
     p = depth_first_bnb(p)
-    print(p.score)
-    plot_protein(p)
+    plot_protein(p, style="paper")
