180175b1c1a718f02311b28d55a927cadd79f479
Okke van Eck <okke.van.eck@gmail.com>
2022-11-20 17:23:16 +0100

    Committing unused overloading code

--- prospr/core/src/breadth_first.cpp
+++ prospr/core/src/breadth_first.cpp
@@ -6,9 +6,41 @@
  */
 
 #include "breadth_first.hpp"
+#include <queue>
 
 
-/* An a_star search function for finding a minimum energy conformation. */
+///* Overloading < operator for Protein objects. */
+//bool operator< (const Protein &protein1, const Protein &protein2) {
+//    return protein1.score < protein2.score ||
+//        (protein1.score == protein2.score && protein1.cur_len > protein2.cur_len);
+//}
+//
+///* Overloading <= operator for Protein objects. */
+//bool operator<= (const Protein &protein1, const Protein &protein2) {
+//    return protein1.score <= protein2.score ||
+//        (protein1.score == protein2.score && protein1.cur_len >= protein2.cur_len);
+//}
+//
+///* Overloading > operator for Protein objects. */
+//bool operator> (const Protein &protein1, const Protein &protein2) {
+//    return protein1.score > protein2.score ||
+//        (protein1.score == protein2.score && protein1.cur_len < protein2.cur_len);
+//}
+//
+///* Overloading >= operator for Protein objects. */
+//bool operator>= (const Protein &protein1, const Protein &protein2) {
+//    return protein1.score >= protein2.score ||
+//        (protein1.score == protein2.score && protein1.cur_len <= protein2.cur_len);
+//}
+
+
+/* A breadth-first search function for finding a minimum energy conformation. */
 Protein* breadth_first(Protein* protein) {
     // TODO: Sort criteria: (energy, nodes_placed) -> equal e, shortest length first
+    std::priority_queue<Protein, std::vector<Protein>, std::greater<Protein>> prioq;
+
+//    Protein
+
+    // Queue entry: <score, len, hash> --> sort by lower
+
 }
