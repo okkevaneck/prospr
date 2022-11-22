ffeacf3c8e8f576480b08e9be5c4b408be6137b7
okkevaneck <okke.van.eck@gmail.com>
2022-11-22 11:32:16 +0100

    Removed symmetry from results

--- prospr/core/src/dijkstra.cpp
+++ prospr/core/src/dijkstra.cpp
@@ -8,6 +8,8 @@
 #include "dijkstra.hpp"
 #include <queue>
 #include <iostream>
+#include <numeric>
+#include <algorithm>
 
 
 /* Definition for a partial fold in the priority queue. */
@@ -23,18 +25,53 @@ struct Conformation {
         this->hash = hash;
     }
 
+    /* Create set of moves depending on current hash. */
+    std::vector<int> _create_move_set(int dim) {
+        /* Construct list of only positive moves. */
+        std::vector<int> pos_moves(dim);
+        std::iota(pos_moves.begin(), pos_moves.end(), 1);
+
+        /* Check if there are any positive moves in current hash. */
+        const auto result = std::find_first_of(this->hash.begin(),
+                                       this->hash.end(),
+                                       pos_moves.begin(),
+                                       pos_moves.end());
+
+        /* Use only negative moves if protein has not left quadrant axes. */
+        if (result == this->hash.end()) {
+            std::vector<int> neg_moves(dim);
+            std::iota(neg_moves.begin(), neg_moves.end(), -dim);
+            std::cout << "Neg moves.\n";
+            return neg_moves;
+        } else {
+            /* Else, create list of all possible moves. */
+            std::vector<int> all_moves(dim * 2 + 1);
+            std::iota(all_moves.begin(), all_moves.end(), -dim);
+            all_moves.erase(all_moves.begin() + dim);
+            std::cout << "All moves.\n";
+            return all_moves;
+        }
+    }
+
     /* Create and return vector of children Conformations. */
     std::vector<Conformation> create_children(Protein* protein) {
+        /* Set hash to check for move validity later on. */
         std::vector<Conformation> children;
-        std::vector<int> moves = {-2, -1, 1, 2};
         protein->set_hash(this->hash);
+
+        /* Construct list of possible moves. Restrict to negative quadrant for
+         * first placements to prevent symmetry.
+         */
+        std::vector<int> moves = this->_create_move_set(protein->get_dim());
 
         for (int m : moves) {
             /* Only add valid conformations to list of children. */
             if (protein->is_valid(m)) {
                 protein->place_amino(m);
 
-                Conformation conf = Conformation(protein->get_score(), protein->get_cur_len(), protein->hash_fold());
+                Conformation conf = Conformation(protein->get_score(),
+                                                 protein->get_cur_len(),
+                                                 protein->hash_fold());
                 children.push_back(conf);
 
                 protein->remove_amino();
@@ -45,14 +82,12 @@ struct Conformation {
     }
 };
 
-
 /* Overload > operator for Conformation in the priority queue.
  * Bigger indicates more potential for leading towards the minimum energy conformation.
  */
 bool operator>(const struct Conformation& conf1, const struct Conformation& conf2) {
     return conf1.score > conf2.score || (conf1.score == conf2.score && conf1.length > conf2.length);
 }
-
 
 /* Overload << operator for printing Conformations. */
 std::ostream &operator<<(std::ostream &os, const Conformation& conf) {
@@ -66,11 +101,19 @@ std::ostream &operator<<(std::ostream &os, const Conformation& conf) {
     return os;
 }
 
-
 /* A breadth-first search function for finding a minimum energy conformation. */
 Protein* dijkstra(Protein* protein) {
-    /* Make priority queue and set initial conformation as only node. */
+    /* A Protein with 3 or less amino acids cannot make a bond, so return. */
+    if (protein->get_sequence().length() <= 3) {
+        protein->place_amino(2);
+        protein->place_amino(2);
+        return protein;
+    }
+
+    /* Make priority queue sorting first on lowest energy, then on smallest length. */
     std::priority_queue<Conformation, std::vector<Conformation>, std::greater<Conformation>> prioq;
+
+    /* Add initial partial conformation as only node in priority queue. */
     Conformation conf = Conformation(0, 1, std::vector<int>{});
     prioq.push(conf);
 
