f929f0c024a620236f31d7dd63e0c3b7c8743d96
Okke van Eck <okke.van.eck@gmail.com>
2023-01-18 15:46:40 +0100

    New version for pypi

--- prospr/__init__.py
+++ prospr/__init__.py
@@ -2,7 +2,7 @@ from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
 from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
 from .visualize import plot_protein
 
-__version__ = "0.2a21"
+__version__ = "0.2a22"
 
 __all__ = [
     "AminoAcid",
