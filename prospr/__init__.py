36931669fb7e0355e82b8e3720966556eda15691
okkevaneck <okke.van.eck@gmail.com>
2023-01-20 12:57:15 +0100

    Retry adding csv and core files

--- prospr/__init__.py
+++ prospr/__init__.py
@@ -2,7 +2,7 @@ from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
 from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
 from .visualize import plot_protein
 
-__version__ = "0.2a27"
+__version__ = "0.2a28"
 
 __all__ = [
     "__version__",
