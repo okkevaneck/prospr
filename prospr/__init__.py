7c9e0417094310c71877d5b88c5ea0d1a5cafbfd
okkevaneck <okke.van.eck@gmail.com>
2023-01-20 12:36:47 +0100

    Upped version for pypi publish

--- prospr/__init__.py
+++ prospr/__init__.py
@@ -2,7 +2,7 @@ from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
 from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
 from .visualize import plot_protein
 
-__version__ = "0.2a24"
+__version__ = "0.2a25"
 
 __all__ = [
     "__version__",
